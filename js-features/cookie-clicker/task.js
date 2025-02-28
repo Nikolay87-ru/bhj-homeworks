b92f7e0ec6307594dc6cfd6d81fed63c0462094d
Neto test <nikolajeliseev834@gmail.com>
2025-02-28 17:19:22 +0300

    Добавил элементы рассчета скорости клика

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,6 +4,12 @@ let count;
 
 const cookie = document.getElementById("cookie");
 
+let clickTime = null;
+
+const speedClickElement = document.getElementById("clicker__speed");
+
+speedClickElement.textContent = 0;
+
 function getCounter() {
   clickerElement = document.getElementById("clicker__counter");
 
@@ -14,24 +20,26 @@ function showCounter() {
   clickerElement.textContent = count;
 }
 
-
 getCounter();
 showCounter();
 
 cookie.onclick = function () {
+  const currentTime = new Date();
+
   count++;
   showCounter();
+
+  if (clickTime) {
+    const timeBetweenClick = (currentTime - clickTime) / 1000;
+    const clicksPerSecond = (1 / timeBetweenClick).toFixed(2);
+    speedClickElement.textContent = clicksPerSecond;
+  }
+
+  clickTime = currentTime;
 
   cookie.width = 210;
 
   setTimeout(() => {
     cookie.width = 200;
   }, 100);
-}
-
-
-
-
-
-
-
+};
