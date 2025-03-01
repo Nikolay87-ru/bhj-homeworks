0288f350f3a0a1fc5469d68ab8226dbda34a35ee
Neto test <nikolajeliseev834@gmail.com>
2025-03-01 13:28:30 +0300

    Обновил код и переместил картинку с печеньем

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,45 +1,45 @@
-let clickerElement;
-
-let count;
-
-const cookie = document.getElementById("cookie");
-
-let clickTime = null;
-
-const speedClickElement = document.getElementById("clicker__speed");
-
-speedClickElement.textContent = 0;
-
-function getCounter() {
-  clickerElement = document.getElementById("clicker__counter");
-
-  count = parseInt(clickerElement.textContent);
-}
-
-function showCounter() {
-  clickerElement.textContent = count;
-}
-
-getCounter();
-showCounter();
-
-cookie.onclick = function () {
-  const currentTime = new Date();
-
-  count++;
-  showCounter();
-
-  if (clickTime) {
-    const timeBetweenClick = (currentTime - clickTime) / 1000;
-    const clicksPerSecond = (1 / timeBetweenClick).toFixed(2);
-    speedClickElement.textContent = clicksPerSecond;
+function startClicker() {
+  let clickerElement;
+  let count;
+  
+  const cookie = document.getElementById("cookie");
+  
+  let clickTime = null;
+  
+  const speedClickElement = document.getElementById("clicker__speed");
+  
+  const getCounter = () => {
+    clickerElement = document.getElementById("clicker__counter");
+    count = parseInt(clickerElement.textContent);
   }
+  
+  const updateCounter = () => {
+    clickerElement.textContent = count;
+  }
+  
+  getCounter();
+  updateCounter();
+  
+  cookie.onclick = function () {
+    const currentTime = new Date();
+  
+    count++;
+    updateCounter();
+  
+    if (clickTime) {
+      const timeBetweenClick = (currentTime - clickTime) / 1000;
+      const clicksPerSecond = (1 / timeBetweenClick).toFixed(2);
+      speedClickElement.textContent = clicksPerSecond;
+    }
+  
+    clickTime = currentTime;
+  
+    cookie.width = 210;
+  
+    setTimeout(() => {
+      cookie.width = 200;
+    }, 100);
+  };
+}
 
-  clickTime = currentTime;
-
-  cookie.width = 210;
-
-  setTimeout(() => {
-    cookie.width = 200;
-  }, 100);
-};
+startClicker();
