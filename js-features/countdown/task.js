10f4b113c0c266d6b6517d24f709f70ad3e851ff
Neto test <nikolajeliseev834@gmail.com>
2025-03-01 13:57:42 +0300

    Небольшие правки

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,45 +1,49 @@
-let timerElement;
-let count;
+function startCountdown() {
+  let timerElement;
+  let count;
 
-function getTimer() {
-  timerElement = document.getElementById("timer");
+  function getTimer() {
+    timerElement = document.getElementById("timer");
 
-  count = parseInt(timerElement.textContent);
+    count = parseInt(timerElement.textContent);
 
-  if (count >= 86400) {
-    count = 86399;
+    if (count >= 86400) {
+      count = 86399;
+    }
   }
-}
 
-function getformatTime(seconds) {
-  const hh = Math.floor(seconds / 3600);
-  const mm = Math.floor((seconds % 3600) / 60);
-  const ss = seconds % 60;
+  function getformatTime(seconds) {
+    const hh = Math.floor(seconds / 3600);
+    const mm = Math.floor((seconds % 3600) / 60);
+    const ss = seconds % 60;
 
-  return [hh, mm, ss]
-    .map((timeElement) => timeElement.toString().padStart(2, "0"))
-    .join(":");
-}
+    return [hh, mm, ss]
+      .map((timeElement) => timeElement.toString().padStart(2, "0"))
+      .join(":");
+  }
 
-const updateformatTime = () => {
-  timerElement.textContent = getformatTime(count);
-};
+  const updateformatTime = () => {
+    timerElement.textContent = getformatTime(count);
+  };
 
-getTimer();
-updateformatTime();
-
-const link = document.querySelector("#link");
-
-const countdown = () => {
-  count--;
-
+  getTimer();
   updateformatTime();
 
-  if (count === 0) {
-    clearInterval(intervalId);
-    alert(`Вы победили в конкурсе!`);
-    link.click();
-  }
-};
+  const link = document.querySelector("#link");
 
-const intervalId = setInterval(countdown, 1000);
+  const countdown = () => {
+    count--;
+
+    updateformatTime();
+
+    if (count === 0) {
+      clearInterval(intervalId);
+      alert(`Вы победили в конкурсе!`);
+      link.click();
+    }
+  };
+
+  const intervalId = setInterval(countdown, 1000);
+}
+
+startCountdown();
