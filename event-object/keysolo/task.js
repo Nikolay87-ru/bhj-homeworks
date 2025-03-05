93af5cf46473982f18a3ab0bcb1251211142a281
Neto test <nikolajeliseev834@gmail.com>
2025-03-05 18:17:13 +0300

    initial commit

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      const inputLetter = event.key?.toLowerCase(); 
+      const currentSymbolText = this.currentSymbol.textContent;
+  
+      if (inputLetter === currentSymbolText) {
+        this.success(); 
+      } else {
+        this.fail(); 
+      }
+    });
   }
 
   success() {
