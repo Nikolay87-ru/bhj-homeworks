4060f96fee76c3c4332d973374fc35b67ae4dc6a
Neto test <nikolajeliseev834@gmail.com>
2025-03-06 12:52:14 +0300

    Добавил таймер

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,9 +1,12 @@
 class Game {
   constructor(container) {
     this.container = container;
-    this.wordElement = container.querySelector('.word');
-    this.winsElement = container.querySelector('.status__wins');
-    this.lossElement = container.querySelector('.status__loss');
+    this.wordElement = container.querySelector(".word");
+    this.winsElement = container.querySelector(".status__wins");
+    this.lossElement = container.querySelector(".status__loss");
+    this.countdownElement = container.querySelector(".status__time");
+    this.currentWord = "";
+    this.timer = null;
 
     this.reset();
 
@@ -14,65 +17,70 @@ class Game {
     this.setNewWord();
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
+    this.updateCountdown();
   }
 
   registerEvents() {
-    document.addEventListener('keydown', (event) => {
-      const inputLetter = event.key?.toLowerCase(); 
+    document.addEventListener("keydown", (event) => {
+      const inputLetter = event.key?.toLowerCase();
       const currentSymbolText = this.currentSymbol.textContent;
-  
+
       if (inputLetter === currentSymbolText) {
-        this.success(); 
+        this.success();
       } else {
-        this.fail(); 
+        this.fail();
       }
     });
   }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
-    this.currentSymbol.classList.add('symbol_correct');
+    // clearInterval(this.timer);
+    if (this.currentSymbol.classList.contains("symbol_current"))
+      this.currentSymbol.classList.remove("symbol_current");
+    this.currentSymbol.classList.add("symbol_correct");
     this.currentSymbol = this.currentSymbol.nextElementSibling;
 
     if (this.currentSymbol !== null) {
-      this.currentSymbol.classList.add('symbol_current');
+      this.currentSymbol.classList.add("symbol_current");
       return;
     }
 
     if (++this.winsElement.textContent === 10) {
-      alert('Победа!');
+      alert("Победа!");
       this.reset();
     }
     this.setNewWord();
   }
 
   fail() {
+    clearInterval(this.timer);
     if (++this.lossElement.textContent === 5) {
-      alert('Вы проиграли!');
+      alert("Вы проиграли!");
       this.reset();
     }
     this.setNewWord();
   }
 
   setNewWord() {
-    const word = this.getWord();
-
-    this.renderWord(word);
+    this.currentWord = this.getWord();
+    this.renderWord(this.currentWord);
+    // this.updateCountdown();
+    this.startCountdown();
   }
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
+        "bob",
+        "awesome",
+        "netology",
+        "hello",
+        "kitty",
+        "rock",
+        "youtube",
+        "popcorn",
+        "cinema",
+        "love",
+        "javascript",
       ],
       index = Math.floor(Math.random() * words.length);
 
@@ -83,14 +91,34 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? "symbol_current" : ""}">${s}</span>`
       )
-      .join('');
+      .join("");
     this.wordElement.innerHTML = html;
 
-    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+    this.currentSymbol = this.wordElement.querySelector(".symbol_current");
+  }
+
+  updateCountdown() {
+    this.countdownElement.textContent = this.currentWord.length;
+  }
+
+  startCountdown() {
+    clearInterval(this.timer);
+
+    let count = this.currentWord.length;
+    this.countdownElement.textContent = count;
+
+    this.timer = setInterval(() => {
+      count--;
+      this.countdownElement.textContent = count;
+
+      if (count === 0) {
+        this.fail();
+        clearInterval(this.timer);
+      }
+    }, 1000);
   }
 }
 
-new Game(document.getElementById('game'))
-
+new Game(document.getElementById("game"));
