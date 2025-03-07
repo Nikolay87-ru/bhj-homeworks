3d7574eabd1fae8e4ec95d48e199dc75eb927708
Neto test <nikolajeliseev834@gmail.com>
2025-03-07 11:54:19 +0300

    Переписан код для функции setActiveTab

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,29 +1,42 @@
 class Tabs {
   constructor(container) {
-      this.container = container;
-      this.tabs = Array.from(container.querySelectorAll('.tab'));
-      this.tabsContent = Array.from(container.querySelectorAll('.tab__content'));
+    this.container = container;
+    this.tabs = Array.from(container.querySelectorAll(".tab"));
+    this.tabsContent = Array.from(container.querySelectorAll(".tab__content"));
 
-      this.initTabs();
+    this.initTabs();
   }
 
   initTabs() {
-      this.tabs.forEach(tab => {
-          tab.addEventListener('click', (event) => this.setActiveTab(event.currentTarget));
-      });
+    this.tabs.forEach((tab) => {
+      tab.addEventListener("click", (event) =>
+        this.setActiveTab(event.currentTarget)
+      );
+    });
   }
 
   setActiveTab(clickedTab) {
-      const indexClickedTab = this.tabs.indexOf(clickedTab);
+    const indexClickedTab = this.tabs.indexOf(clickedTab);
 
-      this.tabs.forEach(tab => tab.classList.remove('tab_active'));
-      this.tabsContent.forEach(content => content.classList.remove('tab__content_active'));
+    this.tabs.forEach((tab) => {
+      if (tab.classList.contains("tab_active")) {
+        tab.classList.remove("tab_active");
+      }
+    });
 
-      activeTab.classList.add('tab_active');
-      this.tabsContent[indexClickedTab].classList.add('tab__content_active');
+    this.tabsContent.forEach((content) => {
+      if (content.classList.contains("tab__content_active")) {
+        content.classList.remove("tab__content_active");
+      }
+    });
+
+    if (indexClickedTab !== -1) {
+      clickedTab.classList.add("tab_active");
+      this.tabsContent[indexClickedTab].classList.add("tab__content_active");
+    }
   }
 }
 
-document.querySelectorAll('.tabs').forEach(tabsContainer => {
+document.querySelectorAll(".tabs").forEach((tabsContainer) => {
   new Tabs(tabsContainer);
 });
