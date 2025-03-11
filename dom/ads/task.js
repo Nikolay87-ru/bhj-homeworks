1fd425a033c7b0130ff098604be914e8f51d07e0
Neto test <nikolajeliseev834@gmail.com>
2025-03-11 12:20:35 +0300

    Еще одна правка

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -4,15 +4,15 @@ class Rotator {
     this.activeCase = this.rotator.querySelector("[data-active]");
     this.activeCase.style.color = this.activeCase.dataset.color;
 
-    this.initRotators();
+    this.initRotator();
   }
 
-  initRotators() {
+  initRotator() {
     const speed = parseInt(this.activeCase.dataset.speed, 10);
 
     setTimeout(() => {
       this.activateRotator();
-      this.initRotators();
+      this.initRotator();
     }, speed);
   }
 
