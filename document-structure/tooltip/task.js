2ba22f128effac31d91f110127ee515db9052409
Neto test <nikolajeliseev834@gmail.com>
2025-03-13 18:12:54 +0300

    Добавил функцию создания подсказки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,27 +2,37 @@ class Tooltip {
   constructor() {
     this.hasTooltip = Array.from(document.querySelectorAll(".has-tooltip"));
 
-    this.initTooltip();
+    this.initTooltipElement();
   }
 
-  initTooltip() {
+  initTooltipElement() {
     this.hasTooltip.forEach((element) => {
       element.addEventListener("click", (e) => {
         e.preventDefault();
-        this.showTooltip(e.currentTarget);
+        this.createTooltip(e.currentTarget);
       });
     });
   }
 
-  showTooltip(clickedText) {
+  createTooltip(clickedText) {
+    if (this.activeTooltip?.dataset.element === clickedText || this.activeTooltip) {
+      this.activeTooltip.remove();
+      return;
+    }
+
     const tooltip = document.createElement("div");
 
-    tooltip.className("tooltip tooltip_active");
+    tooltip.className = "tooltip tooltip_active";
     tooltip.textContent = clickedText.title;
     tooltip.dataset.element = clickedText;
+
+    const tooltipBox = clickedText.getBoundingClientRect();
+    tooltip.style.top = `${tooltipBox.bottom}px`;
+    tooltip.style.left = `${tooltipBox.left}px`;
 
     document.body.append(tooltip);
     this.activeTooltip = tooltip;
   }
 }
+
 new Tooltip(document.body);
