30e7540b622b1cef350afd1b83d64b6185d8b3cb
Neto test <nikolajeliseev834@gmail.com>
2025-03-14 17:06:55 +0300

    Добавил изменение цвета placeholder на красный в случае отправки пустой строки

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -8,20 +8,33 @@ class Todo {
   }
 
   initTasksForm() {
-    this.tasksForm.addEventListener('submit', (e) => {
+    this.tasksForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      const newTask = this.taskInput.value;
-    
-      if (newTask === '') {
-          alert('Please enter a task!');
-          return;
+      const newTask = this.taskInput.value.trim();
+
+      if (!newTask) {
+        this.taskInput.classList.add("error");
+        return;
       }
-    
-      this.taskInput.value = ''; 
+
+      this.taskInput.classList.remove("error");
+      this.taskInput.value = "";
+    });
+
+    this.taskInput.addEventListener("input", () => {
+      if (this.taskInput.value.trim()) {
+        this.taskInput.classList.remove("error");
+      }
     });
   }
 
-  
+  addTask(task) {
+    const listItem = document.createElement("li");
+    const taskText = document.createElement("span");
+    listItem.textContent = task;
+
+    todoList.appendChild(listItem);
+  }
 }
 
 new Todo();
