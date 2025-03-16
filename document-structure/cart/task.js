99df6c840536e64c4f79606d275e1a108d4d5d10
Neto test <nikolajeliseev834@gmail.com>
2025-03-16 18:36:02 +0300

    Начал переписывать код полностью

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,46 +1,29 @@
 class Cart {
-  constructor() {
-    this.product = document.querySelector(".product");
-    this.decItem = document.querySelector(".product__quantity-control_dec");
-    this.incItem = document.querySelector(".product__quantity-control_inc");
-    // this.valueItem = document.querySelectorAll(".product__quantity-value");
-    this.counters = [];
+  constructor(product ) {
+    this.product = product;
+    this.incButton = productElement.querySelector('.product__quantity-control_inc');
+    this.decButton = productElement.querySelector('.product__quantity-control_dec');
+    this.productQuantity = productElement.querySelector('.product__quantity-value');
+    this.value = 1;
 
     this.initCounters();
   }
 
   initCounters() {
-    this.product.forEach((product) => {
-      const id = product.dataset.id;
-      this.counters[id] = 1;
-      product.querySelector(".product__quantity-value").textContent =
-        counters[id];
-    });
+  this.updateCounters();
 
+  this.incButton.addEventListener('click', () => this.changeValue(1));
+  this.decButton.addEventListener('click', () => this.changeValue(-1));
+  }
+
+  changeValue(num) {
+    this.value = Math.max(1, this.value + num);
     this.updateCounters();
   }
 
   updateCounters() {
-    document.addEventListener("click", (e) => {
-      const target = e.target;
-      const container = target.closest(".product");
-
-      if (!container) return;
-
-      const id = container.dataset.id;
-      const decrement = target.classList.contains('product__quantity-control_dec');
-      const increment = target.classList.contains('product__quantity-control_inc');
-
-      if (increment) {
-        counters[id]++;
-      } else if (decrement) {
-        counters[id] = Math.max(0, counters[id] - 1);
-      }
-
-      const product = document.querySelector(`.product[data-id="${id}"]`);
-        if (product) {
-            product.querySelector('.product__quantity-value').textContent = counters[id];
-        }
-    });
+    this.productQuantity.textContent = this.value;
   }
 }
+
+new Cart();
