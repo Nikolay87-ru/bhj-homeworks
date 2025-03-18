3bfca639bc1cf9cd6b530dff2b7f3623c530fc13
Neto test <nikolajeliseev834@gmail.com>
2025-03-18 14:48:30 +0300

    правки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,32 +3,45 @@ class Tooltip {
     this.hasTooltip = document.querySelectorAll(".has-tooltip");
     this.tooltip = document.createElement("div");
     this.tooltip.className = "tooltip";
-    document.body.appendChild(this.tooltip);
-    
+    document.body.append(this.tooltip);
+
     this.activeLink = null;
 
-    this.initTooltip()
+    this.initTooltip();
   }
 
   initTooltip() {
-    this.hasTooltip.forEach(link => {
-      link.addEventListener("click", e => {
+    this.hasTooltip.forEach((link) => {
+      link.addEventListener("click", (e) => {
         e.preventDefault();
         this.toggle(link);
       });
     });
   }
 
-  initTooltipClick() {
-
+  toggle(link) {
+    if (this.activeLink === link) {
+      this.hideTooltip();
+    } else {
+      this.showTooltip(link);
+    }
   }
 
-  createTooltip() {
-
+  showTooltip(link) {
+    this.hideTooltip();
+    this.tooltip.textContent = link.title;
+    const rect = link.getBoundingClientRect();
+    this.tooltip.style.cssText = `
+      top: ${rect.bottom}px;
+      left: ${rect.left}px;
+    `;
+    this.tooltip.classList.add("tooltip_active");
+    this.activeLink = link;
   }
 
-  removeTooltip() {
-
+  hideTooltip() {
+    this.tooltip.classList.remove("tooltip_active");
+    this.activeLink = null;
   }
 }
 
