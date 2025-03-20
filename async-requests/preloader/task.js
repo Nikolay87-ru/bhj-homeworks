fded873fb8df332c8bc0fe221ab85c66fc846ab1
Neto test <nikolajeliseev834@gmail.com>
2025-03-20 19:50:35 +0300

    Дописал функцию

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,5 +1,6 @@
 function getData() {
   const loader = document.querySelector(".loader");
+  const itemsContainer = document.getElementById("items");
   const xhr = new XMLHttpRequest();
   xhr.open(
     "GET",
@@ -9,23 +10,26 @@ function getData() {
   xhr.addEventListener("readystatechange", () => {
     if (xhr.readyState === xhr.DONE) {
       loader.classList.remove("loader_active");
-      createData();
+      const data = JSON.parse(xhr.response);
+      createData(data.response.Valute);
     }
   });
   
-  function createData() {
-    getValues();
+  function createData(valuteData) {
+    itemsContainer.innerHTML = "";
+
+    for (const currencyCode in valuteData) {
+      const currency = valuteData[currencyCode];
+    }
 
     const item = document.querySelector(".item");
     item.innerHTML = `
-      <div class="item__code">${code}</div>
-      <div class="item__value">${value}</div>
-      <div class="item__currency">${currency}</div>
+      <div class="item__code">${currency.CharCode}</div>
+      <div class="item__value">${currency.Value}</div>
+      <div class="item__currency">${currency.Name}</div>
     `;
-  }
 
-  const getValues = () => {
-
+    itemsContainer.append(item);
   }
 
   xhr.send();
