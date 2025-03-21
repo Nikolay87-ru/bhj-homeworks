95e50214399ffc34fb3ac8446113f5f85088a59d
Neto test <nikolajeliseev834@gmail.com>
2025-03-21 12:17:36 +0300

    Правки

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,6 +1,12 @@
 function getData() {
   const loader = document.querySelector(".loader");
-  const itemsContainer = document.getElementById("items");
+  const currencyCache = 'currencyCache';
+
+  const cache = localStorage.getItem(currencyCache);
+  if (cache) {
+    render(JSON.parse(cache));
+  }
+
   const xhr = new XMLHttpRequest();
   xhr.open(
     "GET",
@@ -10,50 +16,35 @@ function getData() {
   xhr.addEventListener("readystatechange", () => {
     if (xhr.readyState === xhr.DONE) {
       loader.classList.remove("loader_active");
-      const data = JSON.parse(xhr.response);
-      createData(data.response.Valute);
+      
+      // if (window.localStorage === null) {
+      //   const data = JSON.parse(xhr.response);
+      //   createData(data.response.Valute);
+      // } 
     }
   });
 
-  function createData(valuteData) {
-    itemsContainer.innerHTML = "";
-
-    for (const currencyCode in valuteData) {
-      const currency = valuteData[currencyCode];
-
-      const item = document.createElement("div");
-      item.classList.add("item");
-
-      item.innerHTML = `
-        <div class="item__code">${currency.CharCode}</div>
-        <div class="item__value">${currency.Value}</div>
-        <div class="item__currency">руб.</div>
-      `;
-
-      itemsContainer.append(item);
-    }
-  }
-
-  function saveDataToLocalStorage() {
-    const data = {};
-    document.querySelectorAll(".items .item").forEach((item) => {
-      const codeText = item.querySelector(".item__code").textContent;
-      const valueText = item.querySelector(".item__value").textContent;
-      const currencyText = item.querySelector(".item__currency").textContent;
-      data.push({ code: codeText, value: valueText, currency: currencyText });
-    });
-
-    localStorage.setItem("data", JSON.stringify(data));
-  }
-
-  function loadDataFromLocalStorage() {
-    const savedData = JSON.parse(localStorage.getItem("data")) || [];
-    savedData.forEach((data) => {
-      createData(data.text);
-    });
-  }
-
   xhr.send();
+}
+
+function createData(valuteData) {
+  const itemsContainer = document.getElementById("items");
+  itemsContainer.innerHTML = "";
+
+  for (const currencyCode in valuteData) {
+    const currency = valuteData[currencyCode];
+
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    item.innerHTML = `
+      <div class="item__code">${currency.CharCode}</div>
+      <div class="item__value">${currency.Value}</div>
+      <div class="item__currency">руб.</div>
+    `;
+
+    itemsContainer.append(item);
+  }
 }
 
 getData();
