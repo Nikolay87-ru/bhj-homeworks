0aec0988bf57da30b065fdc3b4af2e399e9f0db7
Neto test <nikolajeliseev834@gmail.com>
2025-03-23 16:22:21 +0300

    Правки html, css и js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,6 +1,7 @@
 function showProgressBar() {
   const form = document.getElementById("form");
   const progress = document.getElementById("progress");
+  // const progressBar = element.value
   const progressText = document.getElementById("progressCounter");
 
   form.addEventListener("submit", uploadFile);
@@ -10,17 +11,22 @@ function showProgressBar() {
 
     const xhr = new XMLHttpRequest();
     const formData = new FormData(form);
-    
+
+    progress.style.width = '0%';
+    progress.value = 0;
+    progressText.textContent = '0%';
+
     xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
 
     xhr.upload.addEventListener("progress", e => {
-      const percent = e.lengthComputable ? (e.loaded / e.total) * 100 : 0;
-      progress.value = percent;
-      progressText.textContent = percent.toFixed(2) + "%";
-
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.send(new FormData(form));
+      if (e.lengthComputable) {
+        const percent = (e.loaded / e.total) * 100;
+        progress.value = percent; 
+        progress.style.width = `${percent}%`; 
+        progressText.textContent = percent.toFixed(2) + "%";
+      }
     });
+    xhr.send(formData);
   }
 }
 
