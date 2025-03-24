5f4e723b216d9dcba91136fba987c5f9a67921e1
Neto test <nikolajeliseev834@gmail.com>
2025-03-24 17:40:59 +0300

    Небольшие корректировки

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,7 +3,6 @@ function showProgressBar() {
   const progress = document.getElementById("progress");
   const progressText = document.getElementById("progressCounter");
   const progressBar = document.querySelector(".progress-bar");
-  const divLoadIsComplite = document.createElement("div");
 
   form.addEventListener("submit", uploadFile);
 
@@ -13,19 +12,21 @@ function showProgressBar() {
     const xhr = new XMLHttpRequest();
     const formData = new FormData(form);
 
-    progress.style.width = '0%';
+    progress.style.width = "0%";
     progress.value = 0;
-    progressText.textContent = '0%';
+    progressText.textContent = "0%";
+
+    const divLoadIsComplite = document.createElement("div");
     divLoadIsComplite.className = "message";
     divLoadIsComplite.innerHTML = ``;
 
     xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
 
-    xhr.upload.addEventListener("progress", e => {
+    xhr.upload.addEventListener("progress", (e) => {
       if (e.lengthComputable) {
         const percent = (e.loaded / e.total) * 100;
-        progress.value = percent; 
-        progress.style.width = `${percent}%`; 
+        progress.value = percent;
+        progress.style.width = `${percent}%`;
         progressText.textContent = percent.toFixed(2) + "%";
       }
       showMessage(progress.style.width);
@@ -34,7 +35,10 @@ function showProgressBar() {
     function showMessage(progressDone) {
       if (progressDone === "100%") {
         divLoadIsComplite.innerHTML = `<span>Ваш файл успешно загружен!</span>`;
-        progressBar.parentNode.insertBefore(divLoadIsComplite, progressBar.nextSibling);
+        progressBar.parentNode.insertBefore(
+          divLoadIsComplite,
+          progressBar.nextSibling
+        );
         return;
       }
     }
