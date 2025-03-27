12673e9ed90eccf0e709b4ee285918c36f32db5a
Neto test <nikolajeliseev834@gmail.com>
2025-03-27 16:31:31 +0300

    форматирование кода

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,22 +1,26 @@
 function getCookie(name) {
-  let cookie = document.cookie.split("; ").find(row => row.startsWith(name + "="));
+  let cookie = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(name + "="));
   return cookie?.split("=")[1] || null;
 }
 
 function setCookie(name, value, days) {
   expirationDate = new Date(Date.now() + days * 864e5).toUTCString();
-  document.cookie = `${name}=${encodeURIComponent(value)};expiration-date=${expirationDate};path=/`;
+  document.cookie = `${name}=${encodeURIComponent(
+    value
+  )};expiration-date=${expirationDate};path=/`;
 }
 
 function showModal() {
   const modal = document.getElementById("subscribe-modal");
   const closeButton = document.querySelector(".modal__close_times");
 
-  if(!getCookie('modalClosed')) {
+  if (!getCookie("modalClosed")) {
     document.addEventListener("DOMContentLoaded", () => {
-        modal.classList.add("modal_active");
+      modal.classList.add("modal_active");
     });
-}
+  }
 
   closeButton.addEventListener("click", () => {
     modal.classList.remove("modal_active");
